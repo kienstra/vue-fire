96c39f1fac079b3551fed75392d60aebdbe39bb4
Ryan Kienstra <kienstraryan@gmail.com>
2021-08-24 18:36:39 -0500

    Add a Firestore config file to get set up

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,9 @@
 
 <script>
 
+import { auth } from './firebase'
+console.log(auth)
+
 export default {
   name: 'App',
   components: {}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import { firestorePlugin } from 'vuefire'
 
-createApp(App).mount('#app')
+Vue.use(firestorePlugin)
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
