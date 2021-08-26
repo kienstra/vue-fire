ffb486d5b7241db8e87cc9470b5d69a705c2e4df
Ryan Kienstra <kienstraryan@gmail.com>
2021-08-26 16:13:30 -0500

    Bump Vue to 3, update main.js

--- src/main.js
+++ src/main.js
@@ -1,21 +1,13 @@
-import Vue from 'vue'
+import { createApp, VueCompositionApi } from 'vue'
 import App from './App.vue'
-import { firestorePlugin } from 'vuefire'
 import VueRouter from 'vue-router'
 import Home from './components/Home';
-import VueCompositionApi from '@vue/composition-api'
-
-Vue.use(firestorePlugin)
-Vue.use(VueRouter)
-Vue.use(VueCompositionApi)
 
 const router = new VueRouter({
-  routes: [
-    { path: '/', component: Home }
-  ]
-})
+  routes: [{ path: "/", component: Home }],
+});
 
-new Vue({
-  router,
-  render: h => h(App),
-}).$mount('#app')
+createApp(App)
+  .use(router)
+  .use(VueCompositionApi)
+  .mount("#app")
